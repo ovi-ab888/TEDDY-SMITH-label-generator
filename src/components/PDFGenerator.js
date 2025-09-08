658ce5a4f4daa6cb59b36981454dfd7264072993
ovi-ab888 <oviabdulohab@gmail.com>
2025-09-08 20:59:13 +0600

    Update PDFGenerator.js

--- src/components/PDFGenerator.js
+++ src/components/PDFGenerator.js
@@ -1,1 +1,31 @@
+import jsPDF from 'jspdf';
+import { chunkArray } from '../utils/helpers.js';
 
+export class PDFGenerator {
+  constructor() {
+    this.doc = new jsPDF();
+    this.currentY = 20;
+  }
+
+  addPage() {
+    this.doc.addPage();
+    this.currentY = 20;
+  }
+
+  async generatePDF(pages, filename = 'barcodes.pdf') {
+    for (let i = 0; i < pages.length; i++) {
+      if (i > 0) this.addPage();
+      
+      const pageHTML = pages[i];
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = pageHTML;
+      
+      // SVG টেমপ্লেট যোগ করা হবে এখানে
+      // বর্তমানে আমরা HTML content add করতে পারছি
+      this.doc.text(`Page ${i + 1}`, 20, this.currentY);
+      this.currentY += 10;
+    }
+
+    this.doc.save(filename);
+  }
+}
